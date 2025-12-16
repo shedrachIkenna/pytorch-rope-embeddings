ba13e8bdab1c0e746a087f740f4c175e6cab592f
ShedrachIkenna <Shedrach686@gmail.com>
2025-12-16 23:13:24 +0100

    added constructor function defination and description

--- rope_embedding/rope.py
+++ rope_embedding/rope.py
@@ -7,3 +7,8 @@ class RotaryPositionalEmbedding(nn.Module):
     """
     Implements Rotary Positional Embedding (RoPe) using PyTorch 
     """
+
+    def __init__(self, head_dim: int, max_seq_len: int, theta: float = 10000.0):
+        super.__init__()
+
+        
