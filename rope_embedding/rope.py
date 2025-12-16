f6cb6a417d89d2173d2d27a1ae59dfbe7e8e8484
ShedrachIkenna <Shedrach686@gmail.com>
2025-12-16 23:10:47 +0100

    Defined rotary class

--- rope_embedding/rope.py
+++ rope_embedding/rope.py
@@ -3,3 +3,7 @@ import torch.nn as nn
 import math
 from typing import Tuple
 
+class RotaryPositionalEmbedding(nn.Module):
+    """
+    Implements Rotary Positional Embedding (RoPe) using PyTorch 
+    """
